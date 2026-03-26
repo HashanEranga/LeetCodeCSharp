ff82364a979f860b7f91ef1a856d0a52c988dd0e
HashanEranga <ccmrhe@gmail.com>
2026-03-27 00:01:43 +0530

    solved group anagrams

---
+++ LeetCodeCSharp.Tests/Arrays/GroupAnagramsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCodeCSharp.Arrays;
+
+namespace LeetCodeCSharp.Tests.Arrays
+{
+    public class GroupAnagramsTests
+    {
+        private readonly GroupAnagrams _solution = new GroupAnagrams();
+        public GroupAnagramsTests()
+        {
+            _solution = new GroupAnagrams();
+        }
+
+        public static IEnumerable<object[]> GroupAnagramsTestData()
+        {
+            yield return new object[]
+            {
+                new string[] { "eat", "tea", "tan", "ate", "nat", "bat" },
+                new List<IList<string>>
+                {
+                    new List<string> { "bat" },
+                    new List<string> { "nat", "tan" },
+                    new List<string> { "ate", "eat", "tea" }
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GroupAnagramsTestData))]
+        public void GroupAnagramsOptimized_ShouldReturnExpectedResult(string[] input, List<IList<string>> expected)
+        {
+            var result = _solution.GroupAnagramsOptimized(input);
+
+            Assert.Equal(expected.Count, result.Count);
+        }
+    }
+}
---
+++ LeetCodeCSharp/Arrays/GroupAnagrams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeCSharp.Arrays
+{
+    public class GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagramsOptimized(string[] strs)
+        {
+            var anagramsDict = new Dictionary<string, List<string>>();
+            foreach (string item in strs)
+            {
+                var charArray = item.ToCharArray();
+                Array.Sort(charArray);
+                var sortedString = new string(charArray);
+
+                if (!anagramsDict.ContainsKey(sortedString)) anagramsDict[sortedString] = new List<string>();
+                anagramsDict[sortedString].Add(item);
+            }
+
+            var result = new List<IList<string>>();
+            foreach (var item in anagramsDict)
+            {
+                result.Add(item.Value);
+            }
+
+            return result;
+        }
+    }
+}
