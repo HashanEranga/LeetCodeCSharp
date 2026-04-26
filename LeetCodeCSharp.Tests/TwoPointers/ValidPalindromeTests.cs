29633b20dbed6853cd146799c3249fef471a2bd5
HashanEranga <ccmrhe@gmail.com>
2026-04-26 18:41:17 +0530

    solved valid palindrome

---
+++ LeetCodeCSharp.Tests/TwoPointers/ValidPalindromeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCodeCSharp.Tests.TwoPointers
+{
+    public class ValidPalindromeTests
+    {
+        private readonly ValidPalindrome validPalindromeSol;
+
+        public ValidPalindromeTests()
+        {
+            validPalindromeSol = new ValidPalindrome();
+        }
+
+        [Theory]
+        [InlineData("race a car", false)]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData(" ", true)]
+        public void IsValidPalindrome_BaseCaseExample_ReturnTrue(string input, bool expectedResult)
+        {
+            // Act 
+            var result = validPalindromeSol.IsPalindrome(input);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }   
+    
+}
+
---
+++ LeetCodeCSharp/TwoPointers/ValidPalindrome.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCodeCSharp;
+
+public class ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        var preProcessedArr = Regex.Replace(s, @"[^a-zA-Z0-9]", "").ToLower().ToArray();
+
+        for(int i = 0; i < preProcessedArr.Length; i++)
+        {
+            int j = preProcessedArr.Length - i - 1;
+            if(i >= j)
+            {
+                if(preProcessedArr[i] != preProcessedArr[j]) return false;
+            }
+        }
+
+        return true;
+
+    }
+}
