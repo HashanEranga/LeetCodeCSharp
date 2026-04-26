935583e6b9ceac4011b2fe4b9bcbdebe961846c9
HashanEranga <ccmrhe@gmail.com>
2026-04-26 20:32:17 +0530

    solved search a 2d matrix

---
+++ LeetCodeCSharp.Tests/BinarySeach/SearchMatrixTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeCSharp.BinarySearch;
+
+namespace LeetCodeCSharp.Tests;
+
+public class SearchMatrixTests
+{
+    private readonly SearchMatrix searchMatrix;
+
+    public SearchMatrixTests()
+    {
+        searchMatrix = new SearchMatrix();
+    }
+
+    public static IEnumerable<object[]> MatrixData => new List<object[]>
+    {
+        new object[] { new int[][] { [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60] }, 3, true },
+        new object[] { new int[][] { [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60] }, 13, false }
+    };
+
+    [Theory]
+    [MemberData(nameof(MatrixData))]
+    public void BasicCaseInSearchMatrix_SearchMatrixSol_ReturnExpectedBoolean(int[][] matrix, int target, bool expectedValue)
+    {
+        var result = searchMatrix.SearchMatrixSol(matrix, target);
+
+        Assert.Equal(expectedValue, result);
+    }
+}
--- LeetCodeCSharp/BinarySearch/SearchMatrix.cs
+++ LeetCodeCSharp/BinarySearch/SearchMatrix.cs
@@ -2,5 +2,29 @@
 
 public class SearchMatrix
 {
+    public bool SearchMatrixSol(int[][] matrix, int target)
+    {
+        int numRows = matrix.GetLength(0);
+        int numCols = matrix[0].Length;
 
+        int start = 0;
+        int end = numRows * numCols - 1;
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+
+            int row = mid / numCols;
+            int col = mid % numCols;
+
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return false;
+    }
 }
